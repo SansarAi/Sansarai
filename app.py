d33f3fc1376f6f405b22bc2bb17376c1d89b0e74
SansarAi <amandeeprai57@gmail.com>
2025-06-12 14:27:52 +0400

    Update app.py

--- app.py
+++ app.py
@@ -120,4 +120,4 @@ def home():
     return render_template_string(TEMPLATE, rules=rules, report=report)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000)
