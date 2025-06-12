0fbe51e89034dcb7a3318d8ea945805dc920b217
SansarAi <amandeeprai57@gmail.com>
2025-06-12 18:42:04 +0400

    Update app.py

--- app.py
+++ app.py
@@ -101,4 +101,4 @@ def contact(): return render_template("contact.html")
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+app.run(debug=True)
